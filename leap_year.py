46e7ca823aed81b1d1acf82fed9db441f1d8c58c
Ubuntu <ubuntu@ip-172-31-61-46.ec2.internal>
2022-04-19 16:18:09 +0000

    fixed

--- leap_year.py
+++ leap_year.py
@@ -15,8 +15,11 @@ def main():
         # process & output
 
         if year % 4 == 0:
-            if year % 400 == 0:
-                print("It is not a leap year.")
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    print("It is a leap year.")
+                else:
+                    print("It is not a leap year.")
             else:
                 print("It is a leap year.")
         else:
